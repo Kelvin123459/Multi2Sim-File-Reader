372baf7656d73987fa4b7d2b3b88ccc8f9a66f20
Kelvin Garcia <kelvin.garcia5@upr.edu>
2018-05-19 01:04:30 -0400

    Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,26 +14,52 @@ import workbook.Workbook;
 public class Main {
 
 	public static void main(String args[]) throws IOException {
-		Workbook wb = new Workbook( "resultsVectorCPU", 
-				"C:\\Users\\Kelvin\\Desktop\\CIIC4082-Project2\\test", 
-				"outputFiles");
-		Workbook wb2 = new Workbook( "resultsFibCPU", 
-				"C:\\Users\\Kelvin\\Desktop\\CIIC4082-Project2\\test", 
-				"outputFiles");
-		Workbook wb3 = new Workbook( "resultsVectorMEM", 
-				"C:\\Users\\Kelvin\\Desktop\\CIIC4082-Project2\\test", 
-				"outputFiles");
-		Workbook wb4 = new Workbook( "resultsFibMEM", 
-				"C:\\Users\\Kelvin\\Desktop\\CIIC4082-Project2\\test", 
-				"outputFiles");
-		ReadVectors vector = new ReadVectors(wb);
-		ReadFib fib = new ReadFib(wb2);
-		vector.readCPU();
-		fib.readCPU();
-		vector = new ReadVectors(wb3);
-		fib = new ReadFib(wb4);
-		vector.readMEM();
-		fib.readMEM();
-		System.out.println("Sequence Terminated");
+		String inputDir = "C:\\Users\\Kelvin\\Desktop\\CIIC4082-Project2\\test";
+		String outputDir = "outputFiles";
+		Workbook wb = new Workbook("resultsVectorCPU", inputDir, outputDir );
+		Workbook wb2 = new Workbook( "resultsFibCPU", inputDir, outputDir);
+		Workbook wb3 = new Workbook( "resultsVectorMEM", inputDir, outputDir);
+		Workbook wb4 = new Workbook( "resultsFibMEM", inputDir, outputDir);
+		ReadVectors vector;
+		ReadFib fib;
+		System.out.println("Multi2Sim File Reader");
+		if(args.length>0) {
+			if(args[0].equals("1")) {
+				System.out.println("Reading Vector Product CPU");
+				vector = new ReadVectors(wb);
+				vector.readCPU();
+				System.out.println("Sequence Terminated");
+			}
+			if(args[0].equals("2")) {
+				System.out.println("Reading Fibonacci CPU");
+				fib = new ReadFib(wb2);
+				fib.readCPU();
+				System.out.println("Sequence Terminated");
+			}
+			if(args[0].equals("3")) {
+				System.out.println("Reading Vector Product MEM");
+				vector = new ReadVectors(wb3);
+				vector.readMEM();
+				System.out.println("Sequence Terminated");
+			}
+			if(args[0].equals("4")) {
+				System.out.println("Reading Fibonacci MEM");
+				fib = new ReadFib(wb4);
+				fib.readMEM();
+				System.out.println("Sequence Terminated");
+			}
+		}
+		else if(args.length==0) {
+			System.out.println("Reading Files");
+			vector = new ReadVectors(wb);
+			vector.readCPU();
+			fib = new ReadFib(wb2);
+			fib.readCPU();
+			vector = new ReadVectors(wb3);
+			fib = new ReadFib(wb4);
+			vector.readMEM();
+			fib.readMEM();
+			System.out.println("Sequence Terminated");
+		}
 	}
 }
